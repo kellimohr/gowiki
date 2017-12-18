3df8130278d7510ac9c47b4584bb000b22529dfc
Kelli Mohr <kelli.mohr@nordstrom.com>
2017-12-18 13:54:33 -0800

    Set up Ginko before specs.

--- gowiki_test.go
+++ gowiki_test.go
@@ -2,11 +2,19 @@ package main_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	//. "github.com/onsi/gomega"
 
 	. "github.com/kellimohr/gowiki"
 )
 
 var _ = Describe("Gowiki", func() {
 
+	var (
+		page Page
+	)
+
+	BeforeEach(func() {
+		page.Title = "Test"
+		page.Body = []byte{'g', 'o', 'l', 'a', 'n', 'g'}
+	})
 })
